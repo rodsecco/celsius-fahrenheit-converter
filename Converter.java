26b67fe2c3ff57600a487dd202f1f2857f2bc978
rodsecco <rodsecco@gmail.com>
2021-03-16 18:59:05 -0300

    Exercício feito - primeiro commit

---
+++ Converter.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+public class Converter {
+
+    Scanner n = new Scanner(System.in);
+    double value;
+
+    public double temperatura() {
+        
+        System.out.println("Insira aqui a temperatura:");    
+        value = n.nextDouble();
+        return value;
+    }
+
+    public double toCelsius(double value){
+        return (value - 32.0 ) * ( 5.0 / 9.0 );
+    }
+    
+    public double toFahrenheit(double value){
+        return (value * 5.0 / 9.0 ) + 32;
+    }
+    
+    public static void main(String[] args) {
+        System.out.println("Conversor de temperatura!!!");
+        System.out.println("Deseja converter a temperatura para qual unidade de medida? \n 1- Celsius \n 2- Fahrenheit"); 
+        Scanner sc = new Scanner(System.in);
+
+        int choice = sc.nextInt();
+
+        if(choice == 1) {
+            Converter converter = new Converter();
+            double temperature = converter.temperatura();
+            double result = converter.toCelsius(temperature);
+            System.out.println("A temperatura inserida em Celsius e: " + result); 
+        }
+        else if(choice == 2) {
+            Converter converter = new Converter();
+            double temperature = converter.temperatura();
+            double result = converter.toFahrenheit(temperature);
+            System.out.println("A temperatura inserida em Fahrenheit e: " + result);                
+        }
+        else {
+            System.out.println("Insira um opcao valida!");
+        }
+    }
+}
