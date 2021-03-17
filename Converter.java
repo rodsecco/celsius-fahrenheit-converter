a81ff7057f516696ba0e78b2720fc0ffe0382cb9
rodsecco <rodsecco@gmail.com>
2021-03-16 21:57:08 -0300

    Ajustes com o pacote

--- Converter.java
+++ Converter.java
@@ -2,13 +2,12 @@ import java.util.Scanner;
 
 public class Converter {
 
-    Scanner n = new Scanner(System.in);
+    Scanner temperature = new Scanner(System.in);
     double value;
 
-    public double temperatura() {
-        
-        System.out.println("Insira aqui a temperatura:");    
-        value = n.nextDouble();
+    public double getTemperatura() {
+        System.out.print("Insira aqui a temperatura: ");    
+        value = temperature.nextDouble();
         return value;
     }
 
@@ -19,7 +18,7 @@ public class Converter {
     public double toFahrenheit(double value){
         return (value * 5.0 / 9.0 ) + 32;
     }
-    
+
     public static void main(String[] args) {
         System.out.println("Conversor de temperatura!!!");
         System.out.println("Deseja converter a temperatura para qual unidade de medida? \n 1- Celsius \n 2- Fahrenheit"); 
@@ -29,18 +28,19 @@ public class Converter {
 
         if(choice == 1) {
             Converter converter = new Converter();
-            double temperature = converter.temperatura();
+            double temperature = converter.getTemperatura();
             double result = converter.toCelsius(temperature);
             System.out.println("A temperatura inserida em Celsius e: " + result); 
         }
         else if(choice == 2) {
             Converter converter = new Converter();
-            double temperature = converter.temperatura();
+            double temperature = converter.getTemperatura();
             double result = converter.toFahrenheit(temperature);
             System.out.println("A temperatura inserida em Fahrenheit e: " + result);                
         }
         else {
             System.out.println("Insira um opcao valida!");
         }
+        sc.close();
     }
 }
